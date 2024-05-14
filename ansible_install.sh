e608901e01555df313fed7c44f3887218805e398
vrdipu <vrdipu@gmail.com>
2024-05-14 09:02:58 +0530

    Create ansible_install.sh
    Added  Ansible installation File

---
+++ ansible_install.sh
@@ -0,0 +1,8 @@
+#Add Ansible repository
+sudo apt-add-repository ppa:ansible/ansible
+#Run the update command
+sudo apt-get update
+#Install Ansible package
+sudo apt-get install ansible
+#Check Ansible Version
+ansible –version
